7d6521cd2105364a0b765a47d28d98aa6112423f
Danila Anikin <danil-anikin-98@mail.ru>
2023-02-15 09:59:11 +0300

    [HW11]-Anikin

--- src/main/java/ru/mtsbank/anikin/controller/BankInfoController.java
+++ src/main/java/ru/mtsbank/anikin/controller/BankInfoController.java
@@ -1,12 +1,17 @@
 package ru.mtsbank.anikin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Random;
 
 @Controller
 public class BankInfoController {
     @GetMapping("/info")
-    public String showInfo(){
+    public String showInfo(Model model) {
+        Integer amountOfClients = new Random().nextInt();
+        model.addAttribute("amountOfClients", amountOfClients.toString());
         return "info";
     }
 }

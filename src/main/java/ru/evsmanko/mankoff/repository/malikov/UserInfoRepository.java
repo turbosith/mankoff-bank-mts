07dcbd46a2adc466fae9e6cf24ed8c13bc8b2e71
whoopzee1337 <whoopzee228@icloud.com>
2023-02-19 20:52:27 +0300

    malikovHW13

--- src/main/java/ru/evsmanko/mankoff/repository/malikov/UserInfoRepository.java
+++ src/main/java/ru/evsmanko/mankoff/repository/malikov/UserInfoRepository.java
@@ -1,11 +1,8 @@
-package ru.evsmanko.mankoff.repository;
+package ru.evsmanko.mankoff.repository.malikov;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ru.evsmanko.mankoff.entity.UserInfoEntity;
-
-
-import java.util.List;
 
 @Repository
 public interface UserInfoRepository extends CrudRepository<UserInfoEntity, Long> {

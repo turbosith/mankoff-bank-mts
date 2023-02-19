22711e4f36dd9a94cb563375e50a64ec01b0cc6a
Татьяна Смирнова <tanya-smn@mail.ru>
2023-02-19 15:30:23 +0300

    [HW-10] Smirnova

---
+++ src/main/java/ru/mtsbank/smirnova/controller/BalanceController.java
@@ -0,0 +1,42 @@
+package ru.mtsbank.smirnova.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.evsmanko.mankoff.entity.Credit;
+import ru.evsmanko.mankoff.entity.Debit;
+import ru.evsmanko.mankoff.repository.CreditRepository;
+import ru.evsmanko.mankoff.repository.DebitRepository;
+import ru.evsmanko.mankoff.repository.UserRepository;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class BalanceController {
+    private final CreditRepository creditRepository;
+    private final DebitRepository debitRepository;
+    private final UserRepository userRepository;
+
+    @GetMapping("/balans/{userId}")
+    public String getBalance(Model model, @PathVariable("userId") Long userId){
+
+        List<Debit> debits = debitRepository.findAllByUserId(userId);
+        List<Credit> credits = creditRepository.findAllByUserId(userId);
+        double sumDebit=0;
+        double sumCredit=0;
+        for(int i=0; i < debits.size(); i++) {
+            sumDebit+=debits.get(i).getAmount();
+        }
+        for(int i=0; i < credits.size(); i++) {
+            sumCredit+=credits.get(i).getAmount();
+        }
+        double ans = sumDebit - sumCredit;
+        model.addAttribute("User id", userId);
+        model.addAttribute("User name", userRepository.getUserById(userId).getFirstName());
+        model.addAttribute("Current balance", ans);
+        return "balans";
+    }
+}
